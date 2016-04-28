e50f82d3418de21e34ce976385ef5d57b733cf24
serkkz <serega-kkz@Mail.ru>
2016-04-28 20:00:12 +0300

    Update collide.py

--- panda3d_tools/collide.py
+++ panda3d_tools/collide.py
@@ -15,15 +15,15 @@ class Collide(bpy.types.Panel):
 
         if bpy.context.active_object.type == "MESH":
             # Вывод листа с осями верха для модели
-            layout.prop(bpy.context.active_object.hatcher, "collide_type", text="Collide type:")
+            layout.prop(bpy.context.active_object.hatcher, "collide_type", text="Collide type")
             
             if bpy.context.active_object.hatcher.collide_type != 'None':
                 
-                layout.prop(bpy.context.active_object.hatcher, "collide_flag_1", text="Flags 1:")
-                layout.prop(bpy.context.active_object.hatcher, "collide_flag_2", text="Flags 2:")
-                layout.prop(bpy.context.active_object.hatcher, "collide_flag_3", text="Flags 3:")
-                layout.prop(bpy.context.active_object.hatcher, "collide_flag_4", text="Flags 4:")
-                layout.prop(bpy.context.active_object.hatcher, "collide_flag_5", text="Flags 5:")
+                layout.prop(bpy.context.active_object.hatcher, "collide_flag_1", text="Flags 1")
+                layout.prop(bpy.context.active_object.hatcher, "collide_flag_2", text="Flags 2")
+                layout.prop(bpy.context.active_object.hatcher, "collide_flag_3", text="Flags 3")
+                layout.prop(bpy.context.active_object.hatcher, "collide_flag_4", text="Flags 4")
+                layout.prop(bpy.context.active_object.hatcher, "collide_flag_5", text="Flags 5")
                 
         else:
             layout.label(text="no select mesh")
