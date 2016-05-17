914f5b814c3962f3cd081f0f3ac91710101f6f71
Sergei Korotkov <serega-kkz@Mail.ru>
2016-05-18 01:47:57 +0300

    Update include_egg.py

--- panda3d_tools/include_egg.py
+++ panda3d_tools/include_egg.py
@@ -28,6 +28,7 @@ class Include(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'  # В каком окне
     bl_region_type = 'TOOLS'  # Место распаложения
     bl_category = 'Hatcher version 0.1'  # Название вкладки
+    bl_options = {'DEFAULT_CLOSED'}
 
     def draw(self, context):
         
