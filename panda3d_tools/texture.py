2c222d59069ff4caa0863288a8c16762b8013c6f
Sergei Korotkov <serega-kkz@Mail.ru>
2016-10-23 19:15:34 +0300

    Update texture.py

--- panda3d_tools/texture.py
+++ panda3d_tools/texture.py
@@ -9,7 +9,7 @@ class Textures(bpy.types.Panel):
     bl_label = "Texture"  # Имя панели
     bl_space_type = 'VIEW_3D'  # В каком окне
     bl_region_type = 'TOOLS'  # Место распаложения
-    bl_category = 'Hatcher version 0.1'  # Название вкладки
+    bl_category = 'Hatcher version 0.3'  # Название вкладки
     bl_options = {'DEFAULT_CLOSED'}
 
     def draw(self, context):
